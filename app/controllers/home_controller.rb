d73a8f1742fac3011191e9311bd651562683184f
Nandan Pramanik <nandan.pramanik@gmail.com>
2012-02-28 23:02:19 +0530

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def education
+
+  end
+
+  def health
+
+  end
+
+  def contact
+
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Bprds::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match 'education' => 'home#education', :as => :education
+  match 'health' => 'home#health', :as => :health
+  match 'contact_us' => 'home#contact', :as => :contact
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
