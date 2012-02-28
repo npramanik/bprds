892e03489fcf9f7210ab7ce993db7c9897df35ae
Nandan Pramanik <nandan.pramanik@gmail.com>
2012-02-28 20:14:48 +0530

    first commite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bprds::Application.config.secret_token = 'a6b0a71a2278f5954dcb1a7680bed74566ee3ff796d348723fc14cf6fdfd646768c81e3d26fec24290825ac516bc442b32e27861e4bd2222e4b9ce8406c368da'
